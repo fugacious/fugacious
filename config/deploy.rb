903866e3acb6c20fd1874fc733bbafb15889818d
Justin Grevich <jgrevich@gmail.com>
2012-05-26 15:36:35 -0700

    added task to symlink database config.

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,9 +9,10 @@ default_run_options[:pty] = true
 set :rvm_ruby_string, '1.9.3'        # Or whatever env you want it to run in.
 set :application, "fugacio.us"
 set :deploy_to, "/var/rails/#{application}" # I like this location
-set :domain, '192.168.126.136'
+set :domain, 'test.fugacio.us'
 set :keep_releases, 2
 set :repository,  "https://github.com/jgrevich/fugacious.git"
+set :branch, "add_passenger"
 set :scm, :git
 set :user, 'ubuntu'
 
@@ -19,11 +20,21 @@ role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
 role :db,  domain, :primary => true # This is where Rails migrations will run
 
+after 'bundle:install', 'deploy:symlink_db'
+before 'deploy:setup', 'rvm:install_rvm'
+after :deploy, 'deploy:chown'
+
 namespace :deploy do
+
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/"
   end
 
   desc "chown & chmod to www-data"
@@ -31,8 +42,5 @@ namespace :deploy do
     sudo "chown -R ubuntu:www-data #{deploy_to}"
     sudo "chmod -R 775 #{deploy_to}"
   end
+  
 end
-
-before 'deploy:setup', 'rvm:install_rvm'
-after :deploy, 'deploy:chown'
-
