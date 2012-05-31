a982b56899bad57e7bbd9bd7e5060551965e7566
Justin Grevich <jgrevich@gmail.com>
2012-05-30 22:11:31 -0700

    setup deploy for fugacio.us

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
-require "bundler/capistrano"
 # Load RVM's capistrano plugin.    
 require "rvm/capistrano"
+require "bundler/capistrano"
 
 set :rvm_ruby_string, '1.9.3'
 set :rvm_type, :system  # Use system-wide RVM
@@ -9,17 +9,19 @@ default_run_options[:pty] = true
 set :rvm_ruby_string, '1.9.3'        # Or whatever env you want it to run in.
 set :application, "fugacio.us"
 set :deploy_to, "/var/rails/#{application}" # I like this location
-set :domain, '192.168.126.136'
+set :domain, 'fugacio.us'
 set :keep_releases, 2
 set :repository,  "https://github.com/jgrevich/fugacious.git"
 set :scm, :git
 set :user, 'ubuntu'
+set :use_sudo, false
 
 role :web, domain                          # Your HTTP server, Apache/etc
-role :app, domain                          # This may be the same as your `Web` server
-role :db,  domain, :primary => true # This is where Rails migrations will run
+role :app, domain                         # This may be the same as your `Web` server
+role :db,  domain, :primary => true        # This is where Rails migrations will run
 
 after 'bundle:install', 'deploy:symlink_db'
+after 'deploy:setup', 'deploy:chown'
 after 'deploy:setup', 'deploy:add_shared_config'
 after :deploy, 'deploy:chown'
 
