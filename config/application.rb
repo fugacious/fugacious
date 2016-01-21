c27996a27bb5568fcde337775545c381b2e50598
trevormast <trevormast@gmail.com>
2016-01-21 10:56:22 -0800

    Removes hardcoded label in form added for testing
    Explains field_error_proc override in comment

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ Bundler.require(*Rails.groups)
 module Fugacious
   class Application < Rails::Application
 
+    # Overridden due to extraneous div being added to elements with errors
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
         html_tag
     }
