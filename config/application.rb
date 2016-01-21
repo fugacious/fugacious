e0e5146e780255869800b2943d2858e2b6334490
trevormast <trevormast@gmail.com>
2016-01-20 18:45:28 -0800

    Validates max_views to allow only numbers greater than zero
    Updates application class to override ActionView::Base.field_error_proc

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,10 @@ Bundler.require(*Rails.groups)
 
 module Fugacious
   class Application < Rails::Application
+
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+        html_tag
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
