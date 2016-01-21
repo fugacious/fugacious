ab7306be9a6b386f42d73c60206250182250b162
trevormast <trevormast@gmail.com>
2016-01-21 11:00:33 -0800

    Runs rubocop to clean up user_messaging_feature_spec

--- spec/features/user_messaging_feature_spec.rb
+++ spec/features/user_messaging_feature_spec.rb
@@ -1,35 +1,34 @@
 require 'rails_helper'
 
-describe "Message Display" do
+describe 'Message Display' do
   before(:each) do
     visit '/'
-    fill_in "message_body", with: "this is a message"
+    fill_in 'message_body', with: 'this is a message'
   end
 
-  context "when remaining views" do
-    it "displays message" do
-      
-      fill_in "message_max_views", with: 2
+  context 'when remaining views' do
+    it 'displays message' do
+      fill_in 'message_max_views', with: 2
       find('input[name="commit"]').click
 
       expect(page).to have_content 'this is a message'
     end
 
-    it "displays destruction warning" do
-      fill_in "message_max_views", with: 2
+    it 'displays destruction warning' do
+      fill_in 'message_max_views', with: 2
       find('input[name="commit"]').click
 
       expect(page).to have_content 'This message will be destroyed'
     end
   end
 
-  context "when no remaining views" do
+  context 'when no remaining views' do
     before(:each) do
-      fill_in "message_max_views", with: 0
+      fill_in 'message_max_views', with: 0
       find('input[name="commit"]').click
     end
 
-    it "displays message" do
+    it 'displays message' do
       expect(page).to have_content 'this is a message'
     end
 
@@ -37,8 +36,8 @@ describe "Message Display" do
       expect(page).to have_no_content 'This message will be destroyed'
     end
 
-    it "displays flash message" do
-      expect(page).to have_content("This message has been deleted")
+    it 'displays flash message' do
+      expect(page).to have_content('This message has been deleted')
     end
   end
 end
