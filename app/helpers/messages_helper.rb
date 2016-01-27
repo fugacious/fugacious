c67c3b65566cbe0ce0959ae9492e1e451000347e
trevormast <trevormast@gmail.com>
2016-01-27 15:44:27 -0800

    Changes time_left helper method to time_away_in_words

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -14,7 +14,7 @@ module MessagesHelper
     end
   end
 
-  def time_left
+  def time_left_in_words
     distance_of_time_in_words_to_now(@message.created_at + @message.hours.hours, include_seconds: true)
   end
 
