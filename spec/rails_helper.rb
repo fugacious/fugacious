10cf4a5a089ca4b76ea5dc4c97930b042a1df0a9
trevormast <trevormast@gmail.com>
2016-01-29 09:46:24 -0800

    Starts Coveralls and Simplecov earlier in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,13 +1,15 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
 require 'simplecov'
 require 'coveralls'
-require 'factory_girl'
-
 Coveralls.wear!('rails')
 SimpleCov.start('rails')
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+
+require 'factory_girl'
+
+
 
 if ENV['CI']
   SimpleCov.formatter = Coveralls::SimpleCov::Formatter
