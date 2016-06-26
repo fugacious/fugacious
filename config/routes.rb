f76097aabc349d23bf553c4952506e0e3c0111ad
trevormast <trevormast@gmail.com>
2016-06-26 13:44:13 -0700

    Adds sinatra gem to development group
    Only requires sidekiq/web in development

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
-require 'sidekiq/web'
 
 Rails.application.routes.draw do
-	
+
 	post 	 'm', to: 'messages#create', as: 'messages'
 	get    'm/:token', to: 'messages#show', as: 'message'
 	delete 'm/:token', to: 'messages#destroy', as: 'destroy_message'
   root to: 'messages#new'
+
   if ENV['RAILS_ENV'] == 'development'
+    require 'sidekiq/web'
+
     mount Sidekiq::Web, at:'/sidekiq'
   end
 
