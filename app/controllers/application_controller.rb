0b914cf09571bd54902ab55e9d0b35288f9c2a28
Joshua Carp <jm.carp@gmail.com>
2016-10-28 11:30:21 -0400

    Skip csrf on json.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception unless -> { request.format.json? }
 
-    
   rescue_from(ActionController::RoutingError) { render :template => 'layouts/404', :status => 404 }
-	rescue_from(ActionController::InvalidAuthenticityToken) { render :template => 'layouts/422', :status => 422}
-	rescue_from(ActiveRecord::RecordNotFound) { render :template => 'messages/gone', :status => 404 }
-	
+  rescue_from(ActionController::InvalidAuthenticityToken) { render :template => 'layouts/422', :status => 422}
+  rescue_from(ActiveRecord::RecordNotFound) { render :template => 'messages/gone', :status => 404 }
 end
