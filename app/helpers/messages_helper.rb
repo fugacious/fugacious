72c2b4191ebf767ba22b13ef50f87cbd61f85ca5
Scott Weber <stephen.weber@gsa.gov>
2017-01-31 15:12:25 -0500

    Integrating USWDS

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -7,9 +7,9 @@ module MessagesHelper
     options[:message] ||= I18n.t(:"activerecord.errors.message", :default => "Correct the following errors and try again.")
     messages = objects.compact.map { |o| o.errors.full_messages }.flatten
     unless messages.empty?
-      content_tag(:div, :class => "error_messages") do
-        list_items = messages.map { |msg| content_tag(:li, msg) }
-        content_tag(:h2, options[:header_message]) + content_tag(:p, options[:message]) + content_tag(:ul, list_items.join.html_safe)
+        content_tag(:div, :class => "usa-alert usa-alert-error") do
+          list_items = messages.map { |msg| content_tag(:li, msg) }
+          content_tag(:h2, options[:header_message], :class => "usa-alert-heading") + content_tag(:p, options[:message]) + content_tag(:ul, list_items.join.html_safe)
       end
     end
   end
