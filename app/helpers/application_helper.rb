e7a9c2deb63cd5851b06c481862d472cbef7a741
Scott Weber <stephen.weber@gsa.gov>
2017-02-24 15:45:33 -0500

    Use USWDS classes, remove references to old layout styling (#108)
    **Why**: We are going to use the standards instead of bootstrap.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,16 +9,11 @@ module ApplicationHelper
     @show_title
   end
 
-   def bootstrap_class_for flash_type
-    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type] || flash_type.to_s
-  end
-
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do 
-              concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
-              concat message 
-            end)
+      concat(content_tag(:div, content_tag(:div, :class=>"usa-alert-body" ), :class =>"usa-alert usa-alert-error")  do
+          concat message
+      end)
     end
     nil
   end
