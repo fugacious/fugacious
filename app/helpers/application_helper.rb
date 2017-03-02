5d60718b7e3c4d13a63b70fe0e06394df704d29d
Scott Weber <stephen.weber@gsa.gov>
2017-03-02 18:12:16 -0500

    Application updates  (#111)
    - Adding assets for messages
    - Updating layout
    - Adding pages for about and privacy
    - Style updates
    - Adding routes for pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,13 +9,22 @@ module ApplicationHelper
     @show_title
   end
 
-  def flash_messages(opts = {})
+  def uswds_class_for (flash_type)
+    { success: "success", error: "danger", alert: "warning", notice: "info" }[flash_type] || flash_type.to_s
+  end
+
+  def flash_messages
     flash.each do |msg_type, message|
-      concat(content_tag(:div, content_tag(:div, :class=>"usa-alert-body" ), :class =>"usa-alert usa-alert-error")  do
-          concat message
-      end)
+      concat(
+        content_tag(:div, class: "usa-alert usa-alert-#{uswds_class_for(msg_type)}") do
+          content_tag(:div, class: 'usa-alert-body') do
+            content_tag(:h3, class: 'usa-alert-heading') do
+              concat message
+            end
+          end
+        end
+      )
     end
     nil
   end
-
 end
