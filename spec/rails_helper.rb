a13a3e9e43be8017a1a8e4a1fc448904b17de7db
Jessie A. Young <jessieay@users.noreply.github.com>
2017-03-07 07:15:35 -0800

    Fix delete button (#120)
    **How**: Delete link requires jquery / ujs. If you use a button it does
    not. We can update style separately
    
    See https://www.viget.com/articles/delete-in-rails-without-jquery-and-ujs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,62 +1,25 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require 'simplecov'
-require 'coveralls'
-Coveralls.wear!('rails')
-SimpleCov.start('rails')
+require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'missing_translations_helper'
-
 require 'factory_girl'
 
-
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |file| require file }
 
 if ENV['CI']
   SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 else
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new( formatter = SimpleCov::Formatter::HTMLFormatter )
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(SimpleCov::Formatter::HTMLFormatter)
 end
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this pattern with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-
-  # RSpec Rails can automatically mix in different behaviours to your tests
-  # based on their file location, for example enabling you to call `get` and
-  # `post` in specs under `spec/controllers`.
-  #
-  # You can disable this behaviour by removing the line below, and instead
-  # explicitly tag your specs with their type, e.g.:
-  #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
-  #
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
-  config.infer_spec_type_from_file_location!
-
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  # config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.order = :random
-  config.infer_spec_type_from_file_location!
-
   config.include FactoryGirl::Syntax::Methods
+  config.infer_spec_type_from_file_location!
+  config.infer_spec_type_from_file_location!
+  config.use_transactional_fixtures = true
 end
