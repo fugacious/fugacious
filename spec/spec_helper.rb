d8be5f6d837ff3bd7032249edf89796aaba5a7d6
amos <amos@amosarts.com>
2017-10-25 11:47:43 -0700

    Adds user flows formatter to RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,18 @@
-require 'simplecov'
 require 'coveralls'
-
-Coveralls.wear!('rails')
-SimpleCov.start 'rails'
+require 'simplecov'
+require 'webmock/rspec'
 
 ENV['RAILS_ENV'] ||= 'test'
 
-require 'webmock/rspec'
+Coveralls.wear!('rails')
+SimpleCov.start 'rails' do
+  add_filter '/lib/rspec/formatters/user_flow_formatter.rb'
+end
 
 RSpec.configure do |config|
   config.order = :random
   WebMock.disable_net_connect!(allow: [/localhost/, /127\.0\.0\.1/, /codeclimate.com/])
+
+  # Skip user_flow specs in default tasks
+  config.filter_run_excluding user_flow: true
 end
