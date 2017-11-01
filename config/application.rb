3586781d265d1195bd41720c1c2b06c0792904c7
amos <amos@amosarts.com>
2017-11-01 13:03:01 -0700

    Makes the brand editable

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module Fugacious
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    I18n.config.globals[:app_name] = Rails.application.secrets.app_name || 'Fugacious'
   end
 end
