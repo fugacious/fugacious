2d912357f50b3fc75dd44ef8e3c7be7c32cc5fca
amos <amos@amosarts.com>
2017-12-22 11:51:43 -0800

    Removes deprecated cookbooks

--- kitchen/roles/db.rb
+++
@@ -1,3 +0,0 @@
-name "db"
-description "The base role for db servers"
-run_list ["recipe[mysql::server]"]
--- kitchen/roles/web-rails.rb
+++
@@ -1,3 +0,0 @@
-name "web-rails"
-description "The base role for rails web servers"
-run_list ["recipe[rvm_passenger]", "recipe[rvm_passenger::apache2]"]
